ffd92f72ac4bc39d56cd6ac744ca07dc50217831
hanbz <hanbzsky@yahoo.com.tw>
2018-08-21 17:51:08 +0800

    v1.0

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -3,6 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\Exception\InvalidEventRequestException;
+use LINE\LINEBot\Exception\InvalidSignatureException;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
 
 class BotController extends Controller
 {
@@ -10,20 +20,69 @@ class BotController extends Controller
 
     public function __construct()
     {
-        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('6XWM1H47Fvjvw9nnwqGuLFXnIox2Ki5Oa7ZKPgagY/3Ue624DNx36ucyNXPUaQjbwoxZl5ut1+FHS5msnqqd4gEq/iwNZu5p0JTvwDGq0M97/YAcn495WzQCs8y5hYnVToMdVaPkouoCpNXRIxLZbgdB04t89/1O/w1cDnyilFU=');
-        $this->bot  = new \LINE\LINEBot($httpClient, ['channelSecret' => '1b938ca560bc6dd17524c8b9ac4b63dc']);
+        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('LINE_BOT_CHANNEL_ACCESS_TOKEN'));
+        $this->bot  = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_BOT_CHANNEL_SECRET')]);
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-        $response           = $this->bot->replyMessage('<reply token>', $textMessageBuilder);
-        if ($response->isSucceeded()) {
-            echo 'Succeeded!';
-            return;
+        $signature = $request->header(HTTPHeader::LINE_SIGNATURE);
+
+        //get the db content
+        $url = 'https://spreadsheets.google.com/feeds/list/' . env('GOOGLE_SHEET_ID') . '/1/public/values?alt=json';
+
+        $sheets      = json_decode(file_get_contents($url), true);
+        $datas_array = [];
+        foreach ($sheets['feed']['entry'] as $db_entry) {
+            $photo_url = $db_entry['gsx$photourl']['$t'];
+            $title     = $db_entry['gsx$title']['$t'];
+            $url       = $db_entry['gsx$url']['$t'];
+            $keyword   = $db_entry['gsx$keyword']['$t'];
+            array_push($datas_array, compact('photo_url', 'title', 'url', 'keyword'));
         }
 
-// Failed
-        echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+        try {
+            $events = $this->bot->parseEventRequest($request->getContent(), $signature);
+            Log::info($events);
+        } catch (InvalidEventRequestException $e) {
+            return response('Invalid signature', 400);
+        } catch (InvalidSignatureException $e) {
+            return response("Invalid event request", 400);
+        }
+
+        foreach ($events as $event) {
+            if (!($event instanceof MessageEvent)) {
+                Log::info('Non message event has come');
+                continue;
+            }
+
+            if (!($event instanceof TextMessage)) {
+                Log::info('Non tex message has come');
+                continue;
+            }
+
+            $sourceText = $event->getText();
+            $replyMsg   = new TextMessageBuilder("有什麼我可以幫你的嗎？");
+
+            foreach ($datas_array as $data) {
+                foreach (explode(',', $data['keyword']) as $keyword) {
+                    if (mb_strpos($sourceText, $keyword) !== false) {
+                        $action   = array(new UriTemplateActionBuilder('查看詳情', $data['url']));
+                        $button   = new ButtonTemplateBuilder($data['title'], $data['title'], $data['photo_url'], $action);
+                        $replyMsg = new TemplateMessageBuilder($data['title'], $button);
+                        break 2;
+                    }
+                }
+            }
+
+            try {
+                $resp = $this->bot->replyMessage($event->getReplyToken(), $replyMsg);
+                Log::info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
+            } catch (\ReflectionException $e) {
+                Log::info($e);
+            }
+        }
+
+        return response('OK');
     }
 }
