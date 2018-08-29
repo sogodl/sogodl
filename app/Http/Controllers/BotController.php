09d06abd19bab73fb8b4b40242da635e2995b21f
hanbz <hanbzsky@yahoo.com.tw>
2018-08-29 14:25:35 +0800

    test

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -34,10 +34,10 @@ class BotController extends Controller
         $sheets      = json_decode(file_get_contents($url), true);
         $datas_array = [];
         foreach ($sheets['feed']['entry'] as $db_entry) {
-            $photo_url = $db_entry['gsx$photourl']['$t'] ?: "";
-            $title     = $db_entry['gsx$title']['$t'] ?: "";
-            $url       = $db_entry['gsx$url']['$t'] ?: "";
-            $keyword   = $db_entry['gsx$keyword']['$t'] ?: "";
+            $photo_url = $db_entry['gsx$photourl']['$t'] ?? "";
+            $title     = $db_entry['gsx$title']['$t'] ?? "";
+            $url       = $db_entry['gsx$url']['$t'] ?? "";
+            $keyword   = $db_entry['gsx$keyword']['$t'] ?? "";
             array_push($datas_array, compact('photo_url', 'title', 'url', 'keyword'));
         }
 
